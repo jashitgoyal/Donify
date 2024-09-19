4299530f12b64de3df364bbf8a3e8c8fa8fb781e
Jashit Goyal <jashitgoyal@Jashits-Laptop.local>
2024-09-19 23:59:54 +0530

    Added hero section

--- script.js
+++ script.js
@@ -19,16 +19,51 @@ function navrenderer() {
 
 function herorenderer() {
   hero_div.innerHTML = `<div>
-    <div>
-  
-    <a><button>Need Help ?</button></a>
-    <a><button>Wanna Help ?</button></a>
-    </div>
-    <div>
-    Back Ground Image
-    </div>
+  <div class="slideshow-container">
+
+<div class="mySlides fade">
+  <div class="numbertext">1 / 3</div>
+  <img src="img/hero1.jpg" style="width:100%">
+  <div class="text">Caption Text</div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">2 / 3</div>
+  <img src="img/hero2.jpg" style="width:100%">
+  <div class="text">Caption Two</div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">3 / 3</div>
+  <img src="img/hero3.jpg" style="width:100%">
+  <div class="text">Caption Three</div>
+</div>
+
+</div>
   <div>`;
 }
 
 navrenderer();
-herorenderer();
+// herorenderer();
+
+let slideIndex = 0;
+showSlides();
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  console.log(dots);
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
