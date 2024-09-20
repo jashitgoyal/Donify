61e94e874d6422678ab5ec6cac1a1fd3d22ff72a
Jashit Goyal <jashitgoyal@Jashits-Laptop.local>
2024-09-20 07:24:37 +0530

    done with homepage

--- script.js
+++ script.js
@@ -1,69 +1,41 @@
-const navbar_div = document.getElementById("navbar");
-const hero_div = document.getElementById("hero-section");
-const z_div = document.getElementById("z-pattern");
-const features_div = document.getElementById("features");
-const footer_div = document.getElementById("footer");
+"use strict";
 
-function navrenderer() {
-  navbar_div.innerHTML = `
-<div class="nav-flex">
- <div>Nav</div>
- <div classname="nav-flex-inner">
- <a href="#">About Us</a>
- <a href="#">Contact Us</a>
- <a href="#">Join Us</a>
- <a href="#">Login</a>
-</div>
-</div>`;
+/**
+ * navbar toggle
+ */
+
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+
+const navElemArr = [navOpenBtn, navCloseBtn];
+
+for (let i = 0; i < navElemArr.length; i++) {
+  navElemArr[i].addEventListener("click", function () {
+    navbar.classList.toggle("active");
+  });
 }
 
-function herorenderer() {
-  hero_div.innerHTML = `<div>
-  <div class="slideshow-container">
+/**
+ * toggle navbar when click any navbar link
+ */
 
-<div class="mySlides fade">
-  <div class="numbertext">1 / 3</div>
-  <img src="img/hero1.jpg" style="width:100%">
-  <div class="text">Caption Text</div>
-</div>
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
 
-<div class="mySlides fade">
-  <div class="numbertext">2 / 3</div>
-  <img src="img/hero2.jpg" style="width:100%">
-  <div class="text">Caption Two</div>
-</div>
-
-<div class="mySlides fade">
-  <div class="numbertext">3 / 3</div>
-  <img src="img/hero3.jpg" style="width:100%">
-  <div class="text">Caption Three</div>
-</div>
-
-</div>
-  <div>`;
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", function () {
+    navbar.classList.remove("active");
+  });
 }
 
-navrenderer();
-// herorenderer();
+/**
+ * header active when window scrolled down
+ */
 
-let slideIndex = 0;
-showSlides();
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  console.log(dots);
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  window.scrollY >= 50
+    ? header.classList.add("active")
+    : header.classList.remove("active");
+});
